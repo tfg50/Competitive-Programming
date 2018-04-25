1689aac468e6339e90277239a69b300d693ba03e
tfg50 <tfg@cin.ufpe.br>
2018-04-25 10:27:53 -0300

    Update SegTree.cpp
    Complete recoding of the Segment Tree. Now uses iterative version

--- Biblioteca/Data Structures/SegTree.cpp
+++ Biblioteca/Data Structures/SegTree.cpp
@@ -1,158 +1,127 @@
+#include <iostream>
+#include <cstdio>
 #include <vector>
 
-struct node
-{
-	node(){valid = false;}
+typedef long long ll;
 
-	node(int a) //init
-	{
-		valid = true;
-		lazy = false;
+struct LazyContext {
+	LazyContext() {
+		
 	}
-
-	node(node a, node b)
-	{
-		if(!a.valid)
-			*this = b;
-		else if(!b.valid)
-			*this = a;
-		else if(!a.valid && !b.valid)
-			*this = node();
-		else
-		{
-			valid = true;
-			lazy = false;
-			//merge
-		}
+	
+	void reset() {
+		
 	}
-
-	void add_lazy(int &a)
-	{
-		//adicionar lazy com contexto de lazy
-
+	
+	void operator += (LazyContext o) {
+		
 	}
-
-	void add_lazy(node &a)
-	{
-		//adicionar lazy do pai
-	}
-
-	void inc_lazy()
-	{
-		//incrementar com o lazy
-
-		//zerar
-		lazy = false;
-	}
-	//atributos
-	bool valid;
-
-	//lazy
-	bool lazy;
+	
+	// atributes
 };
 
-template <class i_t, class s_t, class lazy_cont = int>
-class Segment_Tree
-{
+struct Node {
+	Node() {
+		// empty constructor
+	}
+	
+	Node() {
+		// init
+	}
+	
+	Node(Node &l, Node &r) {
+		// merge
+		
+	}
+	
+	void apply(LazyContext lazy) {
+		
+	}
+	
+	// atributes
+};
+
+template <class i_t, class e_t, class lazy_cont = int>
+class SegmentTree {
 public:
-	Segment_Tree(std::vector<s_t> _a)
-	{
-		a = _a;
-		n = a.size();
-		st.resize(4*n);
-		build(0, n - 1, 0);
+	void init(std::vector<e_t> base) {
+		n = base.size();
+		h = 0;
+		while((1 << h) < n) h++;
+		tree.resize(2 * n);
+		dirty.assign(n, false);
+		lazy.resize(n);
+		for(int i = 0; i < n; i++) {
+			tree[i + n] = i_t(base[i]);
+		}
+		for(int i = n - 1; i > 0; i--) {
+			tree[i] = i_t(tree[i + i], tree[i + i + 1]);
+		}
 	}
-
-	i_t qry(int i, int j){return qry(0, n-1, i, j, 0);}
-	void upd(int pos, s_t val){upd(0, n-1, pos, 0, val);}
-	void upd(int i, int j, lazy_cont val){upd(0, n-1, i, j, 0, val);}
+	
+	i_t qry(int l, int r) {
+		if(l >= r) return i_t;
+		l += n, r += n;
+		push(l);
+		push(r - 1);
+		i_t lp, rp;
+		for(; l < r; l /= 2, r /= 2) {
+			if(l & 1) lp = i_t(lp, tree[l++]);
+			if(r & 1) rp = i_t(tree[--r], rp);
+		}
+		return i_t(lp, rp);
+	}
+	
+	void upd(int l, int r, lazy_cont lc) {
+		if(l >= r) return;
+		l += n, r += n;
+		push(l);
+		push(r - 1);
+		int l0 = l, r0 = r;
+		for(; l < r; l /= 2, r /= 2) {
+			if(l & 1) apply(l++, lc);
+			if(r & 1) apply(--r, lc);
+		}
+		build(l0);
+		build(r0 - 1);
+	}
+	
 private:
-	int left(int p){return p + p + 1;}
-	int right(int p){return p + p + 2;}
-	std::vector<s_t> a;
-	std::vector<i_t> st;
-	int n;
-
-	void build(int l, int r, int ind)
-	{
-		if(l==r)
-		{
-			st[ind] = i_t(a[l]);
-			return;
-		}
-		int mid = (l + r) / 2;
-		build(l, mid, left(ind));
-		build(mid + 1, r, right(ind));
-		st[ind] = i_t(st[left(ind)], st[right(ind)]);
-	}
-
-	void push(int l, int r, int ind)
-	{
-		if(!st[ind].lazy)
-			return;
-		if(l!=r)
-		{
-			st[left(ind)].add_lazy(st[ind]);
-			st[right(ind)].add_lazy(st[ind]);
-		}
-		st[ind].inc_lazy();
-	}
-
-	void upd(int l, int r, int pos, int ind, s_t &val)
-	{
-		push(l, r, ind);
-		
-		if(l>pos || r<pos)
-			return;
-
-		if(l==r)
-		{
-			a[pos] = val;
-			st[ind] = i_t(a[pos]);
-			return;
-		}
-
-		int mid = (l + r) / 2;
-		upd(l, mid, pos, left(ind), val);
-		upd(mid + 1, r, pos, right(ind), val);
-		st[ind] = i_t(st[left(ind)], st[right(ind)]);
-	}
-
-	void upd(int l, int r, int i, int j, int ind, lazy_cont &val)
-	{
-		push(l, r, ind);
-		
-		if(r<i || l>j)
-			return;
-
-		if(l>=i && r<=j)
-		{
-			st[ind].add_lazy(val);
-			push(l, r, ind);
-		}
-		else
-		{
-			int mid = (l + r) / 2;
-			upd(l, mid, i, j, left(ind), val);
-			upd(mid + 1, r, i, j, right(ind), val);
-			st[ind] = i_t(st[left(ind)], st[right(ind)]);
+	int n, h;
+	std::vector<bool> dirty;
+	std::vector<i_t> tree;
+	std::vector<lazy_cont> lazy;
+	
+	void apply(int p, lazy_cont &lc) {
+		tree[p].apply(lc);
+		if(p < n) {
+			dirty[p] = true;
+			lazy[p] += lc;
 		}
 	}
-
-	i_t qry(int l, int r, int i, int j, int ind)
-	{
-		int mid = (l + r) / 2;
-		push(l, r, ind);
-
-		if(r<i || l>j)
-			return i_t();
-
-		else if(l>=i && r<=j)
-			return st[ind];
-
-		return i_t(
-			qry(l, mid, i, j, left(ind)),
-			qry(mid + 1, r, i, j, right(ind))
-				);
+	
+	void push(int p) {
+		for(int s = h; s > 0; s--) {
+			int i = p >> s;
+			if(dirty[i]) {
+				apply(i + i, lazy[i]);
+				apply(i + i + 1, lazy[i]);
+				lazy[i].reset();
+				dirty[i] = false;
+			}
+		}
+	}
+	
+	void build(int p) {
+		for(p /= 2; p > 0; p /= 2) {
+			tree[p] = i_t(tree[p + p], tree[p + p + 1]);
+			if(dirty[p]) {
+				tree[p].apply(lazy[p]);
+			}
+		}
 	}
 };
+
+int main() {
+	
+}
