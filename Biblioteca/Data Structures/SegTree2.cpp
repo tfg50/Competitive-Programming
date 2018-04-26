b529bc918920ebd6dc7c698763dbf473d199e253
tfg50 <tfg@cin.ufpe.br>
2018-04-26 05:37:33 -0300

    Create SegTree2.cpp
    Point update range query

---
+++ Biblioteca/Data Structures/SegTree2.cpp
@@ -0,0 +1,55 @@
+// Point update, range Query segment tree
+
+#include <vector>
+
+struct Node {
+	Node() {
+		// empty constructor
+	}
+	
+	Node() {
+		// init
+	}
+	
+	Node(Node l, Node r) {
+		// merge
+	}
+
+	// atributes
+};
+
+template <class i_t, class e_t>
+class SegmentTree {
+public:
+	void init(std::vector<e_t> base) {
+		n = base.size();
+		tree.resize(2 * n);
+		for(int i = 0; i < n; i++) {
+			tree[i + n] = i_t(base[i]);
+		}
+		for(int i = n - 1; i > 0; i--) {
+			tree[i] = i_t(tree[i + i], tree[i + i + 1]);
+		}
+	}
+	
+	i_t qry(int l, int r) {
+		i_t lp, rp;
+		for(l += n, r += n; l < r; l /= 2, r /= 2) {
+			if(l & 1) lp = i_t(lp, tree[l++]);
+			if(r & 1) rp = i_t(tree[--r], rp);
+		}
+		return i_t(lp, rp);
+	}
+	
+	void upd(int pos, e_t v) {
+		pos += n;
+		tree[pos] = i_t(v);
+		for(pos /= 2; pos > 0; pos /= 2) {
+			tree[pos] = i_t(tree[pos + pos], tree[pos + pos + 1]);
+		}
+	}
+	
+private:
+	int n;
+	std::vector<i_t> tree;
+};
