8a42657758be1d4f2ed31c73d53b429bc36a8d63
tfg50 <tfg@cin.ufpe.br>
2018-05-27 16:29:01 -0300

    Create ColorUpdate.cpp

---
+++ Biblioteca/Data Structures/ColorUpdate.cpp
@@ -0,0 +1,52 @@
+#include <set>
+#include <vector>
+
+template <class Info = int>
+class ColorUpdate {
+public:
+	struct Range {
+		Range(int l = 0) { this->l = l; }
+		Range(int l, int r, Info v) {
+			this->l = l;
+			this->r = r;
+			this->v = v;
+		}
+		int l, r;
+		Info v;
+
+		bool operator < (const Range &b) const { return l < b.l; }
+	};
+
+	std::vector<Range> upd(int l, int r, Info v) {
+		std::vector<Range> ans;
+		auto it = ranges.lower_bound(l);
+		if(it != ranges.begin()) {
+			it--;
+			if(it->r >= l) {
+				auto cur = *it;
+				ranges.erase(it);
+				ranges.insert(Range(cur.l, l - 1, cur.v));
+				ranges.insert(Range(l, cur.r, cur.v));
+			}
+		}
+		it = ranges.upper_bound(r);
+		if(it != ranges.begin()) {
+			it--;
+			if(it->r > r) {
+				auto cur = *it;
+				ranges.erase(it);
+				ranges.insert(Range(cur.l, r, cur.v));
+				ranges.insert(Range(r + 1, cur.r, cur.v));
+			}
+		}
+		for(it = ranges.lower_bound(l); it != ranges.end() && it->l <= r; it++) {
+			ans.push_back(*it);
+		}
+		ranges.erase(ranges.lower_bound(l), ranges.upper_bound(r));
+
+		ranges.insert(Range(l, r, v));
+		return ans;
+	}
+private:
+	std::set<Range> ranges;
+};
