c898ec4eb4ea862272fbb3e703d8e92009b1fad2
tfg50 <tfg@cin.ufpe.br>
2018-06-07 15:57:15 -0300

    Update Bit2D.cpp

--- Biblioteca/Data Structures/Bit2D.cpp
+++ Biblioteca/Data Structures/Bit2D.cpp
@@ -4,14 +4,11 @@
 
 typedef std::pair<int, int> ii;
 
-struct Bit2D
-{
+struct Bit2D {
 public:
-	Bit2D(std::vector<ii> pts)
-	{
+	Bit2D(std::vector<ii> pts) {
 		std::sort(pts.begin(), pts.end());
-		for(auto a : pts)
-		{
+		for(auto a : pts) {
 			if(ord.empty() || a.first != ord.back())
 				ord.push_back(a.first);
 		}
@@ -20,39 +17,30 @@ public:
 		for(auto &a : pts)
 			std::swap(a.first, a.second);
 		std::sort(pts.begin(), pts.end());
-		for(auto &a : pts)
-		{
+		for(auto &a : pts) {
 			std::swap(a.first, a.second);
-			for(int on = std::upper_bound(ord.begin(), ord.end(), a.first) - ord.begin(); on < fw.size(); on += on & -on)
-			{
+			for(int on = std::upper_bound(ord.begin(), ord.end(), a.first) - ord.begin(); on < fw.size(); on += on & -on) {
 				if(coord[on].empty() || coord[on].back() != a.second);
 					coord[on].push_back(a.second);
 			}
 		}
-		for(int i = 0; i < fw.size(); i++)
-		{
+		for(int i = 0; i < fw.size(); i++) {
 			fw[i].assign(coord[i].size() + 1, 0);
 		}
 	}
 
-	void upd(int x, int y, int v)
-	{
-		for(int xx = std::upper_bound(ord.begin(), ord.end(), x) - ord.begin(); xx < fw.size(); xx += xx & -xx)
-		{
-			for(int yy = std::upper_bound(coord[xx].begin(), coord[xx].end(), y) - coord[xx].begin(); yy < fw[xx].size(); yy += yy & -yy)
-			{
+	void upd(int x, int y, int v) {
+		for(int xx = std::upper_bound(ord.begin(), ord.end(), x) - ord.begin(); xx < fw.size(); xx += xx & -xx) {
+			for(int yy = std::upper_bound(coord[xx].begin(), coord[xx].end(), y) - coord[xx].begin(); yy < fw[xx].size(); yy += yy & -yy) {
 				fw[xx][yy] += v;
 			}
 		}
 	}
 
-	int qry(int x, int y)
-	{
+	int qry(int x, int y) {
 		int ans = 0;
-		for(int xx = std::upper_bound(ord.begin(), ord.end(), x) - ord.begin(); xx > 0; xx -= xx & -xx)
-		{
-			for(int yy = std::upper_bound(coord[xx].begin(), coord[xx].end(), y) - coord[xx].begin(); yy > 0; yy -= yy & -yy)
-			{
+		for(int xx = std::upper_bound(ord.begin(), ord.end(), x) - ord.begin(); xx > 0; xx -= xx & -xx) {
+			for(int yy = std::upper_bound(coord[xx].begin(), coord[xx].end(), y) - coord[xx].begin(); yy > 0; yy -= yy & -yy) {
 				ans += fw[xx][yy];
 			}
 		}
