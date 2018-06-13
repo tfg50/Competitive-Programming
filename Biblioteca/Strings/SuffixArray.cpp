b68f3fec521fece58d6d968c7f0f48ea4158323f
tfg50 <tfg@cin.ufpe.br>
2018-06-13 10:19:05 -0300

    Create SuffixArray.cpp
    O(nlog^2n) construction

---
+++ Biblioteca/Strings/SuffixArray.cpp
@@ -0,0 +1,39 @@
+template <class T>
+class SuffixArray {
+public:
+	std::vector<int> buildSuffix(const std::vector<T> &array) {
+		int n = array.size();
+		std::vector<int> sa(n);
+		for(int i = 0; i < n; i++) {
+			sa[i] = i;
+		}
+		std::sort(sa.begin(), sa.end(), [&](int a, int b) { return array[a] < array[b]; });
+		int cur = 0;
+		std::vector<int> inv(n);
+		std::vector<int> nxt(n);
+		inv[sa[0]] = 0;
+		for(int i = 1; i < n; i++) {
+			inv[sa[i]] = (array[sa[i - 1]] != array[sa[i]] ? ++cur : cur);
+		}
+		for(int k = 0; cur + 1 < n; k++) {
+			cur = 0;
+			auxSort(sa, inv, 1 << k);
+			for(int l = 0, r = 0; l < n; l = r, cur++) {
+				while(r < n && getPair(inv, sa[l], 1 << k) == getPair(inv, sa[r], 1 << k)) {
+					nxt[sa[r++]] = cur;
+				}
+			}
+			nxt.swap(inv);
+		}
+		return sa;
+	}
+private:
+	void auxSort(std::vector<int> &sa, const std::vector<int> &inv, int offset) {
+		// O(nlogn) step, O(nlog^2n) total
+		std::sort(sa.begin(), sa.end(), [&](int a, int b) { return getPair(inv, a, offset) < getPair(inv, b, offset); });
+		// O(n) step, O(nlogn) total -- TO DO --
+	}
+	std::pair<int, int> getPair(const std::vector<int> &inv, int pos, int offset) {
+		return std::pair<int, int>(inv[pos], pos + offset < inv.size() ? inv[pos + offset] : -1);
+	}
+};
