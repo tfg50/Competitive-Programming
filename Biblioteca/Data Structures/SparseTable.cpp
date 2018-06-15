7efb00bb1f5529a06c6d42b637f1ca970a3ac225
tfg50 <tfg@cin.ufpe.br>
2018-06-15 16:24:54 -0300

    Create SparseTable.cpp

---
+++ Biblioteca/Data Structures/SparseTable.cpp
@@ -0,0 +1,35 @@
+struct Merger {
+	int operator() (int a, int b) { return std::min(a, b); }
+};
+
+template <class T, class Merger = std::less<T> >
+class SparseTable {
+public:
+	void init(std::vector<T> a) {
+		int e = 0;
+		int n = a.size();
+		while((1 << e) / 2 < a.size()) {
+			e++;
+		}
+		table.resize(e, std::vector<T>(n));
+		get.assign(n + 1, -1);
+		for(int i = 0; i < n; i++) {
+			table[0][i] = a[i];
+			get[i+1] = get[(i+1)/2] + 1;
+		}
+		for(int i = 0; i + 1 < e; i++) {
+			for(int j = 0; j + (1 << i) < n; j++) {
+				table[i+1][j] = merge(table[i][j], table[i][j + (1 << i)]);
+			}
+		}
+	}
+	
+	T qry(int l, int r) {
+		int e = get[r - l];
+		return merge(table[e][l], table[e][r - (1 << e)]);
+	}
+private:
+	std::vector<std::vector<T>> table;
+	std::vector<int> get;
+	Merger merge;
+};
