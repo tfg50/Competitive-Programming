f619a97dfff5f5557b00f0b18bdd9b35c0b60380
tfg50 <tfg@cin.ufpe.br>
2018-06-22 21:25:11 -0300

    Create FenwickTree.cpp

---
+++ Biblioteca/Data Structures/FenwickTree.cpp
@@ -0,0 +1,38 @@
+template <class T>
+class FenwickTree {
+public:
+	void init(int n) {
+		this->n = n;
+		bit.assign(n + 1, 0);
+	}
+
+	void init(const std::vector<T> &a) {
+		n = a.size();
+		bit.assign(n + 1, 0);
+		for(int i = 1; i <= n; i++) {
+			bit[i] += a[i - 1];
+			if(i + (i & -i) <= n) {
+				bit[i + (i & -i)] += bit[i];
+			}
+		}
+	}
+
+	T qry(int x) {
+		x = std::min(x, (int)bit.size() - 1);
+		T ans = 0;
+		for(; x > 0; x -= x & -x) {
+			ans += bit[x];
+		}
+		return ans;
+	}
+
+	void upd(int x, T v) {
+		if(x <= 0) return;
+		for(; x <= n; x += x & -x) {
+			bit[x] += v;
+		}
+	}
+private:
+	int n;
+	std::vector<T> bit;
+};
