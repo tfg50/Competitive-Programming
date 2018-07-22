45b0da0a81ded3f4cf7fabdf655d4251a2a4ab98
tfg50 <tfg@cin.ufpe.br>
2018-07-22 20:41:13 -0300

    Create MaxQueue.cpp

---
+++ Biblioteca/Data Structures/MaxQueue.cpp
@@ -0,0 +1,35 @@
+template <class T, class C = std::less<T>>
+struct MaxQueue {
+	MaxQueue() {
+		clear();
+	}
+
+	void clear() {
+		id = 0;
+		q.clear();
+	}
+
+	void push(T x) {
+		std::pair<int, T> nxt(1, x);
+		while(q.size() > id && cmp(q.back().second, x)) {
+			nxt.first += q.back().first;
+			q.pop_back();
+		}
+		q.push_back(nxt);
+	}
+
+	T qry() {
+		return q[id].second;
+	}
+
+	void pop() {
+		q[id].first--;
+		if(q[id].first == 0) {
+			id++;
+		}
+	}
+private:
+	std::vector<std::pair<int, T>> q;
+	int id;
+	C cmp;
+};
