602803bd2abaf8173f5cbdba7896b388961962a0
tfg50 <tfg@cin.ufpe.br>
2018-09-14 18:20:04 -0300

    Add files via upload

--- Biblioteca/Strings/Z_algorithm.cpp
+++ Biblioteca/Strings/Z_algorithm.cpp
@@ -1,1 +1,15 @@
+// credits to FMota/UFCG
 
+template <class T>
+struct ZFunc {
+	std::vector<int> z;
+	ZFunc(const std::vector<T> &v) : z(v.size()) {
+		int n = (int) v.size(), a = 0, b = 0;
+		if (!z.empty()) z[0] = n;
+		for (int i = 1; i < n; i++) {
+			int end = i; if (i < b) end = std::min(i + z[i - a], b);
+			while(end < n && v[end] == v[end - i]) ++end;
+			z[i] = end - i; if(end > b) a = i, b = end;
+		}
+	}
+};
