409cbb82f798dd2ef151586c6b7518871ee1ff65
tfg50 <tfg@cin.ufpe.br>
2018-10-16 17:42:44 -0300

    Create temp.cpp
    Adding template

---
+++ Biblioteca/temp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <random>
+#include <limits>
+
+std::mt19937 rng((int) std::chrono::steady_clock::now().time_since_epoch().count());
+
+int main() {
+	std::ios_base::sync_with_stdio(false); std::cin.tie(NULL);
+}
