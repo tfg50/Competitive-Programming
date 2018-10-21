9a2210e01e24ec82406c1a337515d3ddf53ee36b
tfg50 <tfg@cin.ufpe.br>
2018-10-20 21:34:11 -0300

    Update FFT.cpp
    2 in 1 trick and fast integer coeficient multiplication without ntt

--- Biblioteca/Math/FFT.cpp
+++ Biblioteca/Math/FFT.cpp
@@ -55,3 +55,56 @@ CVector fft(CVector a, bool inv = false) {
 	}
 	return a;
 }
+
+void fft2in1(CVector &a, CVector &b) {
+	int n = (int) a.size();
+	for(int i = 0; i < n; i++) {
+		a[i] = Complex(a[i].real, b[i].real);
+	}
+	auto c = fft(a);
+	for(int i = 0; i < n; i++) {
+		a[i] = (c[i] + c[(n-i) % n].conj()) * Complex(0.5, 0);
+		b[i] = (c[i] - c[(n-i) % n].conj()) * Complex(0, -0.5);
+	}
+}
+
+void ifft2in1(CVector &a, CVector &b) {
+	int n = (int) a.size();
+	for(int i = 0; i < n; i++) {
+		a[i] = a[i] + b[i] * Complex(0, 1);
+	}
+	a = fft(a, true);
+	for(int i = 0; i < n; i++) {
+		b[i] = Complex(a[i].imag, 0);
+		a[i] = Complex(a[i].real, 0);
+	}
+}
+
+std::vector<long long> mul(const std::vector<long long> &a, const std::vector<long long> &b, long long cut = 1 << 16) {
+	int n = (int) a.size();
+	std::vector<CVector> C(4, CVector(n, 0));
+	for(int i = 0; i < n; i++) {
+		C[0][i] = a[i] % cut;
+		C[1][i] = a[i] / cut;
+		C[2][i] = b[i] % cut;
+		C[3][i] = b[i] / cut;
+	}
+	fft2in1(C[0], C[1]);
+	fft2in1(C[2], C[3]);
+	for(int i = 0; i < n; i++) {
+		// 00, 01, 10, 11
+		Complex cur[4];
+		for(int j = 0; j < 4; j++) cur[j] = C[j/2+2][i] * C[j % 2][i];
+		for(int j = 0; j < 4; j++) C[j][i] = cur[j];
+	}
+	ifft2in1(C[0], C[1]);
+	ifft2in1(C[2], C[3]);
+	std::vector<long long> ans(n, 0);
+	for(int i = 0; i < n; i++) {
+		// if there are negative values, care with rounding
+		ans[i] += (long long) (C[0][i].real + 0.5);
+		ans[i] += (long long) (C[1][i].real + C[2][i].real + 0.5) * cut;
+		ans[i] += (long long) (C[3][i].real + 0.5) * cut * cut;
+	}
+	return ans;
+}
