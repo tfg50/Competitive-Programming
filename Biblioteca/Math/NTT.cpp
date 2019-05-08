8e10f1b56a30cda6a7ba41a7b61698346dff3350
tfg50 <tfg@cin.ufpe.br>
2019-05-08 07:36:38 -0300

    Update NTT.cpp

--- Biblioteca/Math/NTT.cpp
+++ Biblioteca/Math/NTT.cpp
@@ -1,8 +1,6 @@
 const int MOD = 998244353;
 const int me = 15;
 const int ms = 1 << me;
-//const int MOD = 754974721;
-
 ll fexp(ll x, ll e, ll mod = MOD) {
 	ll ans = 1;
 	x %= mod;
@@ -14,27 +12,45 @@ ll fexp(ll x, ll e, ll mod = MOD) {
 	}
 	return ans;
 }
-
 //is n primitive root of p ?
 bool test(ll x, ll p) {
 	ll m = p - 1;
-	for(int i = 2; i * i <= m; ++i) if(m % i == 0) {
+	for(int i = 2; i * i <= m; ++i) if(!(m % i)) {
 		if(fexp(x, i, p) == 1) return false;
 		if(fexp(x, m / i, p) == 1) return false;
 	}
 	return true;
 }
-
 //find the largest primitive root for p
-ll search(ll p) {
-	for(ll i = p - 1; i >= 2; --i) if(test(i, p)) return i;
+int search(int p) {
+	for(int i = p - 1; i >= 2; --i) if(test(i, p)) return i;
 	return -1;
 }
+map<int, int> roots;
+int get_root(int p) {
+	if(roots[p]) {
+		return roots[p];
+	} else {
+		roots[p]=search(p);
+		return roots[p];
+	}
+}
 
-#define add(x, y, mod) (x+y>=mod?x+y-mod:x+y)
+#define add(x, y) x+y>=MOD?x+y-MOD:x+y
 
 const int gen = search(MOD);
-int bits[ms], r[ms + 1];
+int bits[ms], root[ms];
+
+void initFFT() {
+	root[1] = 1;
+	for(int len = 2; len < ms; len += len) {
+		int z = fexp(gen, (MOD - 1) / len / 2);
+		for(int i = len / 2; i < len; i++) {
+			root[2 * i] = root[i];
+			root[2 * i + 1] = (long long) root[i] * z % MOD;
+		}
+	}
+}
 
 void pre(int n) {
 	int LOG = 0;
@@ -46,37 +62,23 @@ void pre(int n) {
 	}
 }
 
-void pre(int n, int root, int mod) {
+vector<int> fft(vector<int> a, int mod, bool inv = false) {
+	int n = (int) a.size();
 	pre(n);
-	r[0] = 1;
-	for(int i = 1; i <= n; i++) {
-		r[i] = (ll) r[i - 1] * root % mod;
-	}
-}
-
-std::vector<int> fft(std::vector<int> a, int mod, bool inv = false) {
-	int root = gen;
 	if(inv) {
-		root = fexp(root, mod - 2, mod);
+		reverse(a.begin() + 1, a.end());
 	}
-	int n = a.size();
-	root = fexp(root, (mod - 1) / n, mod);
-	pre(n, root, mod);
 	for(int i = 0; i < n; i++) {
 		int to = bits[i];
-		if(i < to) {
-			std::swap(a[i], a[to]);
-		}
+		if(i < to) 
+			swap(a[i], a[to]);
 	}
 	for(int len = 1; len < n; len *= 2) {
 		for(int i = 0; i < n; i += len * 2) {
-			int cur_root = 0;
-			int delta = n / (2 * len);
 			for(int j = 0; j < len; j++) {
-				int u = a[i + j], v = (ll) a[i + j + len] * r[cur_root] % mod;
-				a[i + j] = add(u, v, mod);
-				a[i + j + len] = add(u, mod - v, mod);
-				cur_root += delta;
+				int u = a[i + j], v = (ll) a[i + j + len] * root[len + j] % mod;
+				a[i + j] = add(u, v);
+				a[i + j + len] = add(u, mod - v);
 			}
 		}
 	}
