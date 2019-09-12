d164cbcba582d2e8bb94a69bfee845641bf52e72
tfg50 <tfg@cin.ufpe.br>
2019-09-12 15:50:37 -0300

    Update Minkowski.cpp
    changed code style to mine

--- Biblioteca/Math/2D Geometry/Minkowski.cpp
+++ Biblioteca/Math/2D Geometry/Minkowski.cpp
@@ -1,80 +1,38 @@
-// Creditos a Agnez! valeu
-
-int cmp(ll x, ll y = 0) {
-	return (x <= y) ? (x < y) ? -1 : 0 : 1; }
-struct point{
-	ll x, y;
-	point():x(0),y(0){}
-	point(ll X, ll Y):x(X),y(Y){}
-	int cmp(point a){
-        if(::cmp(x,a.x))
-            return ::cmp(x,a.x);
-        return ::cmp(y,a.y);
-    }
-    point operator+(point a){ return point(x+a.x, y+a.y); }
-    point operator-(point a){ return point(x-a.x, y-a.y); }
-    point operator*(ll a){ return point(x*a,y*a); }
-    point operator/(ll a){ return point(x/a,y/a); }
-    ll operator*(point a){ return x*a.x + y*a.y; }
-    ll operator%(point a){ return x*a.y - y*a.x; }
-    bool operator==(point a){ return cmp(a)==0; }
-    bool operator!=(point a){ return cmp(a)!=0; }
-    bool operator<=(point a){ return cmp(a)<=0; }
-};
-
-bool operator < (point a, point b) {
-	return a.cmp(b) == -1;
-}
-
-int ccw(point a, point b, point c){ return cmp((b-a)%(c-a)); }
- 
-typedef vector<point> polygon;
-point pivot;
-bool radialcomp(point p, point q){
-	point P = p-pivot, Q = q-pivot;
-	ll R = P%Q;
-	if(cmp(R)) return R > 0;
-	return cmp(P*P,Q*Q) < 0;
-}
- 
-bool comp(point a, point b){
-	if((a.x > 0 || (a.x==0 && a.y>0) ) && (b.x < 0 || (b.x==0 && b.y<0))) return 1;
-	if((b.x > 0 || (b.x==0 && b.y>0) ) && (a.x < 0 || (a.x==0 && a.y<0))) return 0;
-	ll R = a%b;
+bool comp(PT a, PT b){
+	int hp1 = (a.x < 0 || (a.x==0 && a.y<0));
+	int hp2 = (b.x < 0 || (b.x==0 && b.y<0));
+	if(hp1 != hp2) return hp1 < hp2;
+	long long R = a%b;
 	if(R) return R > 0;
 	return a*a < b*b;
 }
- 
-polygon poly_sum(polygon &a, polygon &b){
-	//Lembre de nao ter pontos repetidos
-	//	passar poligonos ordenados
-	//	se nao tiver pontos colineares, pode usar:
-	//pivot = *min_element(all(a));
-	//sort(all(a),radialcomp);
-	//a.resize(unique(all(a))-a.begin());
-	//pivot = *min_element(all(b));
-	//sort(all(b),radialcomp);
-	//b.resize(unique(all(b))-b.begin());
-	if(!sz(a) || !sz(b)) return polygon(0);
-	if(min(sz(a),sz(b)) < 2){
-		polygon ret(0);
-		rep(i,0,sz(a)) rep(j,0,sz(b)) ret.pb(a[i]+b[j]);
+
+std::vector<PT> minkowskiSum(const std::vector<PT> &a, const std::vector<PT> &b){
+	if(a.empty() || b.empty()) return std::vector<PT>(0);
+	std::vector<PT> ret;
+	int na = (int) a.size(), nb = (int) b.size();
+	if(std::min(a.size(), b.size()) < 2){
+		for(int i = 0; i < na; i++) {
+			for(int j = 0; j < nb; j++) {
+				ret.push_back(a[i]+b[j]);
+			}
+		}
 		return ret;
 	}
-	polygon ret;
-	ret.pb(a[0]+b[0]);
+	ret.push_back(a[0]+b[0]);
+	PT p = ret.back();
 	int pa = 0, pb = 0;
-	while(pa < sz(a) || pb < sz(b)){
-		point p = ret.back();
-		if(pb == sz(b) || (pa < sz(a) && comp((a[(pa+1)%sz(a)]-a[pa]),(b[(pb+1)%sz(b)]-b[pb]))))
-			p = p + (a[(pa+1)%sz(a)]-a[pa]), pa++;
-		else p = p + (b[(pb+1)%sz(b)]-b[pb]), pb++;
-		//descomentar para tirar pontos colineares (o poligono nao pode ser degenerado)
-//		while(sz(ret) > 1 && !ccw(ret[sz(ret)-2], ret[sz(ret)-1], p)) 
-//			ret.pop_back();
-		ret.pb(p);
+	while(pa + pb + 1 < na + nb) {
+		PT va = (a[(pa+1)%na]-a[pa%na]);
+		PT vb = (b[(pb+1)%nb]-b[pb%nb]);
+		if(pb == nb || (pa < na && comp(va, vb))) { p = p + va, pa++; }
+		else { p = p + vb, pb++; }
+		while(ret.size() >= 2 && (p-ret.back()) % (p-ret[(int)ret.size()-2]) == 0) {
+			// removing colinear points
+			// needs the scalar product stuff it the result is a line
+			ret.pop_back();
+		}
+		ret.push_back(p);
 	}
-	assert(ret.back() == ret[0]);
-	ret.pop_back();
 	return ret;
 }
