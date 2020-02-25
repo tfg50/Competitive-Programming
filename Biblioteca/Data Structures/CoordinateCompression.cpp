640da85bc17d1b980e6e386e86ac012600c0c22a
tfg50 <tfg@cin.ufpe.br>
2020-02-25 19:23:08 -0300

    Create CoordinateCompression.cpp

---
+++ Biblioteca/Data Structures/CoordinateCompression.cpp
@@ -0,0 +1,14 @@
+template<class T>
+class CoordinateCompression {
+public:
+	CoordinateCompression(const std::vector<T> &a) {
+		arr = a;
+		std::sort(arr.begin(), arr.end());
+		arr.resize(std::unique(arr.begin(), arr.end()) - arr.begin());
+	}
+
+	int getL(T x) { return std::lower_bound(arr.begin(), arr.end(), x) - arr.begin(); }
+	int getR(T x) { return std::upper_bound(arr.begin(), arr.end(), x) - arr.begin(); }
+private:
+	std::vector<T> arr;
+};
