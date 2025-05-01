b61105d70aa64371c00d0639857c17ed30994d4c
tfg50 <tfg50br@gmail.com>
2025-05-01 08:34:27 -0300

    adding template file

---
+++ Biblioteca/template.cpp
@@ -0,0 +1,10 @@
+#include "Biblioteca/template_basics.hpp"
+
+int main() {
+    std::ios_base::sync_with_stdio(false); std::cin.tie(NULL);
+    int t;
+    std::cin >> t;
+    while(t--) {
+        
+    }
+}
